04e4395ae6ffa03a07eb71ebae7a8cf530a9f186
Diel Oliveira de Faria <diel@localhost.localdomain>
2013-12-14 10:34:18 -0200

    projeto iniciado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SIVIC::Application.config.secret_key_base = '1302577645d7ae79561bcfc781bce567b74a4b29415d280eed772513d1433098269ac9aea67659e16e64f1c4cf80b0a474502c84c5560ff40eb186017dba7496'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SIVIC::Application.config.session_store :cookie_store, key: '_SIVIC_session'
