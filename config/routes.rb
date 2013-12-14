3e0fc812f6d6269751e335afebbe4a8a416ad859
Diel Oliveira de Faria <diel@localhost.localdomain>
2013-12-14 11:11:08 -0200

    scaffolding  profissoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SIVIC::Application.routes.draw do
+  resources :sivic_profissaos
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
