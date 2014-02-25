0a6de5c5152c35f78c7cf4006a63a9bc8fee141f
Diel Oliveira de Faria <diel@localhost.localdomain>
2014-02-24 23:46:15 -0200

    tree

--- app/helpers/sivic_discipulos_helper.rb
+++ app/helpers/sivic_discipulos_helper.rb
@@ -1,2 +1,32 @@
 module SivicDiscipulosHelper
+
+	def BuscaPessoas(id)
+
+
+	sivic_pessoas = SivicPessoa.where(:father_id => id) rescue nil
+
+		if sivic_pessoas
+
+			html = '<ul>'
+
+			sivic_pessoas.each do |sivic_pessoas|
+
+			html += '<li>'
+
+				html += '<a href="#">' + sivic_pessoas.NOME_pessoa + '</a>'
+
+				html +=BuscaPessoas(sivic_pessoas.id)
+
+			html += '</li>'
+
+			end
+
+			html += '</ul>'
+		end
+
+		#debugger
+
+		html.html_safe
+	end
+
 end
