0b4db5f5bd69f75034efb90ac6d98183acd05757
Diel Oliveira <diel.faria@gmail.com>
2014-05-19 13:22:59 -0300

    relatorios celulas

--- db/migrate/20140226212722_create_sivic_situacoesrelatorios.rb
+++ db/migrate/20140226212722_create_sivic_situacoesrelatorios.rb
@@ -9,7 +9,7 @@ class CreateSivicSituacoesrelatorios < ActiveRecord::Migration
     	SivicSituacoesrelatorio.create(:DESC_Situacao => "ENVIADO")
 	    SivicSituacoesrelatorio.create(:DESC_Situacao => "PENDENTE")
 	    SivicSituacoesrelatorio.create(:DESC_Situacao => "RECEBIDO")  
-	    SivicSituacoesrelatorio.create(:DESC_Situacao => "ATRASADO")  
+	    SivicSituacoesrelatorio.create(:DESC_Situacao => "ATRASADO")
 	    SivicSituacoesrelatorio.create(:DESC_Situacao => "DISPONIVEL")  
   end
 end
