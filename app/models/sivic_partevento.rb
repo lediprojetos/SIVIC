e5179402760e5843d1ec27434223d96dc90d878e
Diel Oliveira <diel.faria@gmail.com>
2014-06-09 12:46:34 -0300

    validando tela de inscricao

--- app/models/sivic_partevento.rb
+++ app/models/sivic_partevento.rb
@@ -3,5 +3,8 @@ class SivicPartevento < ActiveRecord::Base
   belongs_to :sivic_evento
   belongs_to :sivic_movimentofinanceiro
 
+  validates :sivic_pessoa_id, :presence => { :message => ' - Escolha uma pessoa!' }
+  validates :sivic_evento_id, :presence => { :message => ' - Escolha uma evento!' }
+
    accepts_nested_attributes_for :sivic_movimentofinanceiro, allow_destroy: true
 end
