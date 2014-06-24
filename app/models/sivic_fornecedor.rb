fcef2ef544723b5047478b978381139b902b43c3
Leandro Pereira Gomes <lpgomes@gmail.com>
2014-06-24 17:37:24 -0300

    alteração tela de fornecedor

--- app/models/sivic_fornecedor.rb
+++ app/models/sivic_fornecedor.rb
@@ -2,4 +2,9 @@ class SivicFornecedor < ActiveRecord::Base
   belongs_to :sivic_endereco
   belongs_to :sivic_igreja
   belongs_to :user
+
+    belongs_to :sivic_endereco, :dependent => :destroy
+
+    accepts_nested_attributes_for :sivic_endereco, allow_destroy: true
+
 end
