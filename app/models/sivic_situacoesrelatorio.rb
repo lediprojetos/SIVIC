021ac5c9dac4426e0145e583b189e07c141e4372
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2014-07-18 22:16:25 -0300

    ainda alterando relatorio

--- app/models/sivic_situacoesrelatorio.rb
+++ app/models/sivic_situacoesrelatorio.rb
@@ -1,14 +1,6 @@
 class SivicSituacoesrelatorio < ActiveRecord::Base
 	has_many :sivic_relatorioscelulas
 
-
-# Criar metodo para cadastro movimento financiero
-# Regras: Quando o sivic_situacoesrelatorio_id = 3 então deve inserir um novo movimento financeiro
-# com o valor do relatorio, pegar o id do movimento financeiro e inserir no relatorio
-
-#esse metedo deve funcionar na alteração do relatorio
-
-
 end
 
 
--- db/migrate/20140505105228_rename_sivic_pessoas_my_column_nome_pessoa_by_hand.rb
+++ db/migrate/20140505105228_rename_sivic_pessoas_my_column_nome_pessoa_by_hand.rb
@@ -1,9 +1,9 @@
 class RenameSivicPessoasMyColumnNomePessoaByHand < ActiveRecord::Migration
-  def self.up
-    rename_column :sivic_pessoas, :NOME_pessoa, :nome_pessoa
-  end
+ # def self.up
+ #   rename_column :sivic_pessoas, :NOME_pessoa, :nome_pessoa
+ # end
 
-  def self.down
-    rename_column :sivic_pessoas, :nome_pessoa, :NOME_pessoa
-  end
+ # def self.down
+ #   rename_column :sivic_pessoas, :nome_pessoa, :NOME_pessoa
+ # end
 end
