e4f2296b36b887d2c73382c0d9d4d51233420ff5
Leandro Pereira Gomes <lpgomes@gmail.com>
2014-07-24 14:02:06 -0300

    funcionalidade turmas

--- app/models/sivic_professor.rb
+++ app/models/sivic_professor.rb
@@ -2,4 +2,11 @@ class SivicProfessor < ActiveRecord::Base
   belongs_to :sivic_pessoa
   belongs_to :inclusao, :class_name => "User", :foreign_key => "user_inclusao"	
   belongs_to :exclusao, :class_name => "User", :foreign_key => "user_bloqueio" 
+
+
+
+  def nome_professor
+     self.sivic_pessoa.nome_pessoa
+  end
+
 end
