92c1155d921b3aede2f24e7c13de6eb97683cc47
Leandro Pereira Gomes <lpgomes@gmail.com>
2014-08-05 17:26:06 -0300

    terminando tela de inscrição

--- app/models/sivic_turmaaluno.rb
+++ app/models/sivic_turmaaluno.rb
@@ -3,4 +3,9 @@ class SivicTurmaaluno < ActiveRecord::Base
   belongs_to :sivic_pessoa
   belongs_to :inclusao, :class_name => "User", :foreign_key => "user_inclusao"	
   belongs_to :bloqueio, :class_name => "User", :foreign_key => "user_bloqueio" 
+
+
+  validates :sivic_pessoa_id, :presence => { :message => ' - Escolha um aluno' }
+  validates :sivic_turma_id, :presence => { :message => ' - Escolha uma turma' }
+
 end
