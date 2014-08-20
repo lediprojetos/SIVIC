e50f156c6ac86fe46b168be71671b41e23690889
Diel Oliveira <diel.faria@gmail.com>
2014-08-20 16:58:49 -0300

    mexendo no layout

--- app/models/sivic_estado.rb
+++ app/models/sivic_estado.rb
@@ -1,3 +1,17 @@
 class SivicEstado < ActiveRecord::Base
-	has_many :sivic_cidades
+	has_many :sivic_cidade
+	validates :sivic_cidade, :presence => true
+
+
+before_destroy  :before_destroy 
+
+def before_destroy
+	if SivicCidade.find_by_sivic_estado_id(id)
+	  errors.add :base, "Cannot delete booking with payments"
+	  false
+  	else
+  		return true
+	end
+end
+
 end
