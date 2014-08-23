8cea2a49ed87253c6f015602f18f35413eba9a05
Diel <diel.faria@gmail.com>
2014-08-23 19:07:08 -0300

    error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ SIVIC::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -67,7 +67,7 @@ SIVIC::Application.configure do
   #                               all/scaffolds/css)
 
 
-  config.assets.paths << Rails.root.join("app","assets","fonts")
+  #config.assets.paths << Rails.root.join("app","assets","fonts")
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
