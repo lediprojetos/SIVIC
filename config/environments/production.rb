0049ecb3aeb9ea9b91f8c8c057af10023ca4788d
Diel <diel.faria@gmail.com>
2014-08-23 17:25:30 -0300

    error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,9 +59,9 @@ SIVIC::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-   config.assets.precompile += %w(*.js, *.css )
+  #config.assets.precompile += %w(*.js, *.css )
 
-   config.assets.paths << Rails.root.join("app","assets","fonts")
+   config.assets.paths << Rails.root.join("app","assets","")
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
