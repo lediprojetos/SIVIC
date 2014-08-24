3c8d934db33926ff1d4480637e604cf4ffe31739
Diel <diel.faria@gmail.com>
2014-08-24 08:58:45 -0300

    error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,7 @@ SIVIC::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(*.js application-all.css application-template.css template/isotope.css 
-                                 template/fancybox/jquery.fancybox.css template/bootstrap.css 
-                                 template/bootstrap-theme.css template/style.css 
-                                 template/default.css all/custom.css 
-                                 all/demo_table.css all/pagination.css 
-                                 all/scaffolds/css)
+  #config.assets.precompile += %w(*.js *.css *.scss)
 
 
   config.assets.paths << Rails.root.join("app","assets","fonts")
