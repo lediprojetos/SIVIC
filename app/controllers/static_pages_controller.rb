53625d987aeeaf0c802ace34ca529504b0fd48d4
Diel <diel.faria@gmail.com>
2014-08-31 09:13:23 -0300

    statipages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,14 +2,14 @@ class StaticPagesController < ApplicationController
 
   def home
 
-    @total_discipulos = SivicDiscipulo.joins(:sivic_pessoa).where('sivic_pessoas.sivic_igreja_id = ?',current_user.sivic_pessoa.sivic_igreja_id).count
-    @total_celulas = SivicCelula.joins(:sivic_pessoa).where('sivic_pessoas.sivic_igreja_id = ?',current_user.sivic_pessoa.sivic_igreja_id).count
+    @total_discipulos = SivicDiscipulo.joins(:sivic_pessoa).where('sivic_pessoas.sivic_igreja_id = ?',current_user.sivic_pessoa.sivic_igreja_id).count rescue nil
+    @total_celulas = SivicCelula.joins(:sivic_pessoa).where('sivic_pessoas.sivic_igreja_id = ?',current_user.sivic_pessoa.sivic_igreja_id).count rescue nil
 
-    @sivic_lancamentos = find_by_Extrato.order(:data_vencimento)
+    @sivic_lancamentos = find_by_Extrato.order(:data_vencimento) rescue 0
     
-    @total_recebimento = find_by_ContasReceber.sum(:valr_pago, :conditions => {:flag_pago => true})
-    @total_pagamento = find_by_ContasPagar.sum(:valr_pago, :conditions => {:flag_pago => true})
-    @total_periodo = find_by_Extrato.sum(:valr_pago)
+    @total_recebimento = find_by_ContasReceber.sum(:valr_pago, :conditions => {:flag_pago => true}) rescue 0
+    @total_pagamento = find_by_ContasPagar.sum(:valr_pago, :conditions => {:flag_pago => true}) rescue 0
+    @total_periodo = find_by_Extrato.sum(:valr_pago) rescue 0
   end
 
   def help
