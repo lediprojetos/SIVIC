67d500e91575634fba7b1ab2ebdf08a4abc80137
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2014-09-02 23:11:02 -0300

    continuando..

--- app/models/sivic_turma.rb
+++ app/models/sivic_turma.rb
@@ -3,4 +3,8 @@ class SivicTurma < ActiveRecord::Base
   belongs_to :inclusao, :class_name => "User", :foreign_key => "user_inclusao"	
   belongs_to :bloqueio, :class_name => "User", :foreign_key => "user_bloqueio" 
   belongs_to :sivic_curso
+
+
+  validates :DESC_turma, :presence =>{ :message => 'Digite a descricao da turma'}
+
 end
