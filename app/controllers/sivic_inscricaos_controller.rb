47301d07f557f32cdefc7979323a10bf6c9c8ac5
Diel <diel.faria@gmail.com>
2014-09-07 01:39:57 -0300

    inscricao

--- app/controllers/sivic_inscricaos_controller.rb
+++ app/controllers/sivic_inscricaos_controller.rb
@@ -1,10 +1,13 @@
+#encoding: utf-8
 class SivicInscricaosController < ApplicationController
   before_action :set_sivic_inscricao, only: [:show, :edit, :update, :destroy]
 
   # GET /sivic_inscricaos
   # GET /sivic_inscricaos.json
   def index
-    @sivic_inscricaos = SivicInscricao.all
+    respond_to do |format|
+      format.html { redirect_to new_user_session_path, notice: 'Por favor realize o login.' }
+    end
   end
 
   # GET /sivic_inscricaos/1
@@ -24,11 +27,44 @@ class SivicInscricaosController < ApplicationController
   # POST /sivic_inscricaos
   # POST /sivic_inscricaos.json
   def create
+
     @sivic_inscricao = SivicInscricao.new(sivic_inscricao_params)
 
     respond_to do |format|
       if @sivic_inscricao.save
-        format.html { redirect_to @sivic_inscricao, notice: 'Sivic inscricao was successfully created.' }
+        
+        @igreja = SivicIgreja.new
+        @igreja.NOME_igreja = sivic_inscricao_params["nome_igreja"]
+        @igreja.NUMR_telefone = sivic_inscricao_params["desc_telefone"]
+        @igreja.NOME_responsavel = sivic_inscricao_params["nome_pessoa"]
+        @igreja.save
+
+        @pessoa = SivicPessoa.new
+        @pessoa.nome_pessoa = sivic_inscricao_params["nome_pessoa"]
+        @pessoa.DESC_email = sivic_inscricao_params["desc_email"]
+        @pessoa.sivic_igreja_id = @igreja.id
+        @pessoa.save
+
+        @User = User.new
+        @User.sivic_pessoa_id = @pessoa.id
+        @User.email = sivic_inscricao_params["desc_email"]
+        @User.password = sivic_inscricao_params["desc_senha"]
+        @User.password_confirmation = sivic_inscricao_params["desc_confirmasenha"]
+        @User.role = "ADMINISTRADOR"
+        @User.save
+
+        @SivicContcelula = SivicContcelula.new
+        @SivicContcelula.NUMR_Contador = 0
+        @SivicContcelula.sivic_igreja_id = @igreja.id
+        @SivicContcelula.save
+        
+        @SivicContdiscipulo = SivicContdiscipulo.new
+        @SivicContdiscipulo.NUMR_Contador = 0
+        @SivicContdiscipulo.sivic_igreja_id = @igreja.id
+        @SivicContdiscipulo.save
+
+
+        format.html { redirect_to new_user_session_path, notice: 'Registro efetuado com sucesso. Por favor realize o login.' }
         format.json { render action: 'show', status: :created, location: @sivic_inscricao }
       else
         format.html { render action: 'new' }
@@ -69,6 +105,6 @@ class SivicInscricaosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sivic_inscricao_params
-      params.require(:sivic_inscricao).permit(:nome_igreja, :nome_pessoa, :desc_telefone, :numg_cidade, :desc_email, :desc_senha, :desc_confirmasenha)
+      params.require(:sivic_inscricao).permit(:desc_confirmaemail, :nome_igreja, :nome_pessoa, :desc_telefone, :numg_cidade, :desc_email, :desc_senha, :desc_confirmasenha)
     end
 end
--- app/models/sivic_inscricao.rb
+++ app/models/sivic_inscricao.rb
@@ -1,22 +1,45 @@
 #encoding: utf-8
 class SivicInscricao < ActiveRecord::Base
 
-#Presence
-validates :nome_igreja, :presence => { :message => ' - Digite um nome para a igreja.' }
-validates :nome_pessoa, :presence => { :message => ' - Digite um nome para a pessoa.' }
-validates :numg_cidade, :presence => { :message => ' - Escolha uma Cidade.' }
-validates :desc_telefone, :presence => { :message => ' - Digite um telefone.' }
-validates :numg_cidade, :presence => { :message => ' - Digite um telefone.' }
-validates :desc_email, :presence => { :message => ' - Digite um email.' }
-validates :desc_confirmaemail, :presence => { :message => ' - Digite a confirmação do email.' }
-validates :desc_senha, :presence => { :message => ' - Digite a senha.' }
-validates :desc_confirmasenha, :presence => { :message => ' - Digite a confirmação da senha.' }
+	#Presence
+	validates :nome_igreja, :presence => { :message => 'Digite um nome para a igreja.' }
+	validates :nome_pessoa, :presence => { :message => 'Digite um nome para a pessoa.' }
+	validates :numg_cidade, :presence => { :message => 'Escolha uma Cidade.' }
+	validates :desc_telefone, :presence => { :message => 'Digite um telefone.' }
+	validates :numg_cidade, :presence => { :message => 'Digite um telefone.' }
+	validates :desc_email, :presence => { :message => 'Digite um email.' }
+	validates :desc_confirmaemail, :presence => { :message => 'Digite a confirmação do email.' }
+	validates :desc_senha, :presence => { :message => 'Digite a senha.' }
+	validates :desc_confirmasenha, :presence => { :message => 'Digite a confirmação da senha.' }
 
-#iguality
-validate :check_email_and_confirmation
 
-def check_email_and_confirmation
-  	errors.add(:desc_confirmaemail, "O email não foi confirmado corretamente.") if :desc_email != :desc_confirmaemail
-end
+	#iguality
+		validate :check_email_and_confirmation
+		validate :check_senha_and_confirmation
+		validate :busca_email_existente
+
+	#length
+		validates_length_of :desc_senha, :minimum => 8, :maximum => 128, :message => "A senha deve ter entre 8 e 128 caracteres."
+		validates_length_of :nome_igreja, :minimum => 4, :message => "O nome da igreja deve conter pelo menos 4 caracteres."
+		validates_length_of :nome_pessoa, :minimum => 4, :message => "O nome da pessoa deve conter pelo menos 4 caracteres."
+
+	#valid
+		validates_length_of :desc_telefone, :minimum => 13, :message => "O telefone digitado não é válido."
+		validates_format_of :desc_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "O email digitado não é válido."
+
+	def busca_email_existente
+		errors.add(:desc_email, "Email já está sendo utilizado.") if User.find_by_email(self.desc_email)
+	end		
+
+	def check_email_and_confirmation
+	  	errors.add(:desc_confirmaemail, "O email não foi confirmado corretamente.") if  self.desc_email != self.desc_confirmaemail
+	end
+
+	def check_senha_and_confirmation
+	  	errors.add(:desc_confirmasenha, "A senha não foi confirmado corretamente.") if self.desc_senha != self.desc_confirmasenha
+	end
+
+
+
 
 end
