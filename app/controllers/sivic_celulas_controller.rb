4bf9ca73f70b401d69d522123eac506ae849fb7a
Diel Oliveira <diel.faria@gmail.com>
2014-09-16 10:10:03 -0300

    lancameneto de job

--- app/controllers/sivic_celulas_controller.rb
+++ app/controllers/sivic_celulas_controller.rb
@@ -8,7 +8,7 @@ class SivicCelulasController < ApplicationController
 
     #@sivic_celulas = SivicCelula.find(:all, :order => :id)
 
-    @sivic_celulas = SivicCelula.where("sivic_igreja_id = ?",current_user.sivic_pessoa.sivic_igreja_id).paginate(:page => params[:page], :per_page => 10)
+    @sivic_celulas = SivicCelula.where("sivic_igreja_id = ?",current_user.sivic_pessoa.sivic_igreja_id).paginate(:page => params[:page], :per_page => 10).order(:id)
 
    # @sivic_eventos = SivicEvento.where("data_encerramento is null and sivic_igreja_id = ?", current_user.sivic_pessoa.sivic_igreja_id).paginate(:page => params[:page], :per_page => 10)
 
--- app/controllers/sivic_relatorioscelulas_controller.rb
+++ app/controllers/sivic_relatorioscelulas_controller.rb
@@ -9,7 +9,7 @@ class SivicRelatorioscelulasController < ApplicationController
       
       SivicRelatorioscelula.update_all( "sivic_situacoesrelatorio_id = 4"," sivic_situacoesrelatorio_id = 5")
 
-      @celulas = SivicCelula.where(:NUMR_Dia => params[:NUMR_Dia])
+      @celulas = SivicCelula.where(:NUMR_Dia => params[:NUMR_Dia],:flag_gerarelatorio => true)
 
       @celulas.each do |sivic_celula|
 
