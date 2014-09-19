c04e9a89bcf875672464ae4d83e088b043f4697a
Diel Oliveira <diel.faria@gmail.com>
2014-09-19 10:15:56 -0300

    error

--- db/migrate/20140918124735_rename_sivic_discipulos_desc_sexo_by_hand.rb
+++ db/migrate/20140918124735_rename_sivic_discipulos_desc_sexo_by_hand.rb
@@ -5,7 +5,6 @@ class RenameSivicDiscipulosDescSexoByHand < ActiveRecord::Migration
     rename_column :sivic_discipulos, :FLAG_Discipulo, :flag_discipulo
     rename_column :sivic_discipulos, :FLAG_Discipulador, :flag_discipulador
     rename_column :sivic_discipulos, :FLAG_Consolidador, :flag_consolidador
-    rename_column :sivic_discipulos, :DATA_Batismo, :data_batismo
 	#Primeiro Nome antigo, depois nome novo
   end
 
@@ -15,7 +14,6 @@ class RenameSivicDiscipulosDescSexoByHand < ActiveRecord::Migration
     rename_column :sivic_discipulos, :flag_discipulo, :FLAG_Discipulo
     rename_column :sivic_discipulos, :flag_discipulador, :FLAG_Discipulador
     rename_column :sivic_discipulos, :flag_consolidador, :FLAG_Consolidador
-    rename_column :sivic_discipulos, :data_batismo, :DATA_Batismo    
     #Primeiro Nome novo, depois nome antigo
   end
 end
