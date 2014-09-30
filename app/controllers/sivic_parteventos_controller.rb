2b94b9973d076251a4030847b0391c9d7a177802
Leandro Pereira Gomes <lpgomes@gmail.com>
2014-09-30 18:28:50 -0300

    relatorio

--- app/controllers/sivic_parteventos_controller.rb
+++ app/controllers/sivic_parteventos_controller.rb
@@ -126,7 +126,7 @@ class SivicParteventosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sivic_partevento_params
-      params.require(:sivic_partevento).permit(:desc_convidadopor, :sivic_pessoa_id, :sivic_evento_id, :FLAG_naoparticipou, :flag_passando, :sivic_igreja_id, :pessoa_convidou_id, sivic_movimentofinanceiro_attributes: [ :id, :VALR_movimento, :user_inclusao, :FLAG_baixa, :sivic_tipmovfinanceiro_id, :sivic_evento_id, :DATA_exclusao, :user_exclusao, :DESC_movimento, :valr_restante ])
+      params.require(:sivic_partevento).permit(:desc_convidadopor, :sivic_pessoa_id, :sivic_evento_id, :FLAG_naoparticipou, :flag_passando, :sivic_igreja_id, :pessoa_convidou, sivic_movimentofinanceiro_attributes: [ :id, :VALR_movimento, :user_inclusao, :FLAG_baixa, :sivic_tipmovfinanceiro_id, :sivic_evento_id, :DATA_exclusao, :user_exclusao, :DESC_movimento, :valr_restante ])
     end
   end    
 
@@ -179,10 +179,20 @@ class SivicParteventosController < ApplicationController
     tasks.each do |task|
       report.list.add_row do |row|
         row.values lblNome: task.sivic_pessoa.nome_pessoa rescue nil
-        row.values lblLider: task.sivic_pessoa.father.nome_pessoa rescue nil
-        row.values lblConvidadoPor: task.desc_convidadopor rescue nil
-        row.values lblValorPago: number_to_currency(task.sivic_movimentofinanceiro.VALR_movimento, unit: "R$", separator: ",", delimiter: "") rescue nil
-        row.values lblValorRestante: number_to_currency(task.sivic_movimentofinanceiro.valr_restante, unit: "R$", separator: ",", delimiter: "") rescue nil
+        row.values lblConvidadoPor: task.convidou.nome_pessoa rescue nil
+
+        @convidou = busca_participante_convidou(task.sivic_evento_id,task.pessoa_convidou)
+
+        row.values lblVlrPagoPart: number_to_currency(task.sivic_movimentofinanceiro.VALR_movimento, unit: "R$", separator: ",", delimiter: "") rescue nil
+        row.values lblVlrResPart: number_to_currency(task.sivic_movimentofinanceiro.valr_restante, unit: "R$", separator: ",", delimiter: "") rescue nil
+
+        row.values lblValorPago: number_to_currency(@convidou.first.sivic_movimentofinanceiro.VALR_movimento, unit: "R$", separator: ",", delimiter: "") rescue nil
+        row.values lblValorRestante: number_to_currency(@convidou.first.sivic_movimentofinanceiro.valr_restante, unit: "R$", separator: ",", delimiter: "") rescue nil
+
+        @lider_geracao = busca_lider_geracao(task.sivic_pessoa_id)
+
+        row.values lblLider: @lider_geracao.sivic_pessoa.nome_pessoa rescue nil
+
         row.values lblCont: cont
 
         cont += 1
@@ -210,3 +220,32 @@ class SivicParteventosController < ApplicationController
                                    disposition: ''
   end
 
+
+
+  def busca_participante_convidou(id_evento,id_pessoa)
+
+       @sivic_partevento = SivicPartevento.where(sivic_evento_id: id_evento, sivic_pessoa_id: id_pessoa)
+       return @sivic_partevento
+  end
+
+
+  def busca_lider_geracao(id_pessoa)
+
+  loop do   
+    @sivic_pessoa = SivicPessoa.find(id_pessoa)    
+    @sivic_discipulo = SivicDiscipulo.find_by sivic_pessoa_id: id_pessoa 
+
+    if @sivic_discipulo
+      if @sivic_discipulo.flag_discipulador?
+          break 
+        else
+        id_pessoa = @sivic_discipulo.sivic_pessoa.father_id  
+      end
+    else
+       id_pessoa = @sivic_pessoa.father_id  
+   end
+   end   
+
+   return @sivic_discipulo
+
+  end
