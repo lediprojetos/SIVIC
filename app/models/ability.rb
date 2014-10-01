da4d158c7f3eeb8f1284e25b276d1625d9cff048
unknown <01188297180@ipasgo18474.ipasgo.go.gov.br>
2014-10-01 18:43:38 -0300

    trabalhando em permissoes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,6 +13,28 @@ class Ability
      cannot :manage, SivicEscolaridade if user.role == "ADMINISTRADOR"
 
 
+     #@sivic_permissaos = SivicUserpermissao.where(:user_id => user.id)
+
+     #@sivic_permissaos.each do | permissao | 
+
+
+      #SivicCelula
+      #if permissao.sivic_permissao.sivic_model.nome_model == 'SivicCelula'
+        #if permissao.sivic_permissao.mumr_acao == 1
+          #can :manage, SivicCelula
+        #end
+      #end
+
+      #SivicMinisterio
+      #if permissao.sivic_permissao.sivic_model.nome_model == 'SivicMinisterio'
+        #if permissao.sivic_permissao.mumr_acao == 1
+          #can :manage, SivicMinisterio
+        #end
+      #end      
+
+
+    #end
+
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
